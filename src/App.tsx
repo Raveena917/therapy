23541b080c42d75ff54e3f55ce65c51b268c6565
raveena917 <raveena@5bix.in>
2024-09-30 15:37:27 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Header from './components/Header';
 import Landingpage from './pages/Landingpage';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
